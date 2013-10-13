8638c3cba83ab1d98c2171869baeca159d9fdd61
suite117 <suite117@gmail.com>
2013-10-13 14:50:02 +0200

    listaprodotti

--- views/store/listaprodotti.php
+++ views/store/listaprodotti.php
@@ -4,39 +4,30 @@ $prodotti		=$products->get_products(); //Array di prodotti
 $product_count 	= count($prodotti);
 print_r($prodotti) ;
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<link rel="stylesheet" type="text/css" href="css/style.css">
-<title>Members</title>
-</head>
-<body>
-	<div id="container">
-		<?php // include 'includes/menu.php';?>
-		<h1>Tutti i prodotti</h1>
-		<p>
-			Ci sono <strong><?php echo $product_count; ?> </strong> prodotti
-			inseriti.
-		</p>
 
-		<p>I link non funzionano per ora</p>
-		<?php 
+<div id="container">
+	<?php // include 'includes/menu.php';?>
+	<h1>Tutti i prodotti</h1>
+	<p>
+		Ci sono <strong><?php echo $product_count; ?> </strong> prodotti
+		inseriti.
+	</p>
 
-		foreach ($prodotti as $prodotto) {
+	<p>I link non funzionano per ora</p>
+	<?php 
+
+	foreach ($prodotti as $prodotto) {
 			$nome = $prodotto['nome'];
 			?>
 
-		<p>
-			<a href="profile.php?email=<?php echo $nome; ?>"><?php echo $nome?> </a>
-			Inserito il:
-			<?php //echo date('F j, Y', $prodotto['time']) ?>
-		</p>
+	<p>
+		<a href="profile.php?email=<?php echo $nome; ?>"><?php echo $nome?> </a>
+		Inserito il:
+		<?php //echo date('F j, Y', $prodotto['time']) ?>
+	</p>
 
-		<?php
+	<?php
 		}
 
 		?>
-	</div>
-</body>
-</html>
+</div>
