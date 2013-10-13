6307a2128870c89bb5faef7abafc1747c0600a2d
suite117 <suite117@gmail.com>
2013-10-13 21:16:17 +0200

    login e contact

--- views/login.php
+++ views/login.php
@@ -1,18 +1,20 @@
 <?php
-require 'views/core/init.php';
+
 $general->logged_in_protect();
+
 if(empty($_POST)===false) {
 	$email=trim($_POST['email']);
-	//$password=trim($_POST['password']);
-	if(empty($email)===true||empty($password)===true) {
-		$errors[]='Sorry, but we need your email and password.';
+	$password=$_POST['password'];
+	if(empty($email) || empty($password)) {
+		$errors[]='Devi inserire le tua email e la tua password.';
 	} else if($users->email_exists($email)===false) {
-		$errors[]='Sorry that username doesn\'t exists.';
+		$errors[]='Spiacenti l\'utente non esiste.';
 	}/* else if($users->email_confirmed($username)===false) {
 	$errors[]='Sorry, but you need to activate your account.  //per ora tutti gli utenti sono attivi di default
 	Please check your email.';
 	}*/ else {
 	$login=$users->login($email,$password);
+
 	if($login===false) {
 		$errors[]='Spiacenti, username e/o password non validi';
 	} else {
@@ -27,18 +29,23 @@ if(empty($_POST)===false) {
 }
 ?>
 
-<?php
-if(empty($errors)===false) {
-echo '<p>'.implode('</p><p>',$errors).'</p>';
-}
-?>
 <div class="container">
+	<?php
+	// check for a successful form post
+	if (isset($success))
+		echo "<div class=\"alert alert-success\">" . $success . "</div>";
+
+	// check for a form error
+	elseif (isset($errors))
+	foreach ($errors as $error)
+		echo "<div class=\"alert alert-danger\">" . $error . "</div>";
+	?>
 
 	<form role="form" method="post" action="">
 		<div class="form-group">
 			<label for="email">Indirizzo Email</label> <input type="email"
 				class="form-control" name="email" id="email"
-				placeholder="Iserisci la tua email">
+				placeholder="Iserisci la tua email" value="<?=@$email ?>">
 		</div>
 		<div class="form-group">
 			<label for="password">Password</label> <input type="password"
@@ -46,12 +53,6 @@ echo '<p>'.implode('</p><p>',$errors).'</p>';
 				placeholder="La tua password">
 		</div>
 		<div class="form-group">
-			<div class="col-md-1">
-				<div class="checkbox">
-					<label> <input type="checkbox"> Ricordami
-					</label>
-				</div>
-			</div>
 			<div class="col-md-1">
 				<button type="submit" name="submit" class="btn btn-default">Accedi</button>
 			</div>
--- views/register.php
+++ views/register.php
@@ -1,5 +1,5 @@
 <?php
-require 'core/init.php';
+
 # if form is submitted
 if (isset($_POST['submit'])) {
 	if (empty($_POST['password']) || empty($_POST['email']) || empty($_POST['partitaiva']) || empty($_POST['ragionesociale'])) {
@@ -29,113 +29,135 @@ if (isset($_POST['submit'])) {
 	}
 }
 if (isset($_GET['success']) && empty($_GET['success'])) {
-	echo 'Grazie per esserti registrato. Please check your email.';
+	$success = 'Grazie per esserti registrato. Please check your email.';
 }
 ?>
 
 <div class="container">
-	
+	<?php
+	// check for a successful form post
+	if (isset($success))
+		echo "<div class=\"alert alert-success\">" . $success . "</div>";
+
+	// check for a form error
+	elseif (isset($errors))
+	foreach ($errors as $error)
+		echo "<div class=\"alert alert-danger\">" . $error . "</div>";
+	?>
+
+
 	<form class="form-horizontal" role="form" method="post" action="">
-		
-		 <div class="form-group">
-      		<label for="firstname" class="col-md-2">Nome:</label>
-      		<div class="col-md-10">
-        		<input type="text" class="form-control" id="firstname" placeholder="Inserisci il tuo nome">
-      		</div> 
-    	</div>
- 
-	    <div class="form-group">
-	      <label for="lastname" class="col-md-2">
-	        Cognome:
-	      </label>
-	      <div class="col-md-10">
-	        <input type="text" class="form-control" id="lastname" placeholder="Inserisci il tuo cognome">
-	      </div>
-	    </div>
-		
+
+		<div class="form-group">
+			<label for="firstname" class="col-md-2">Nome:</label>
+			<div class="col-md-10">
+				<input type="text" class="form-control" id="firstname"
+					placeholder="Inserisci il tuo nome">
+			</div>
+		</div>
+
+		<div class="form-group">
+			<label for="lastname" class="col-md-2"> Cognome: </label>
+			<div class="col-md-10">
+				<input type="text" class="form-control" id="lastname"
+					placeholder="Inserisci il tuo cognome">
+			</div>
+		</div>
+
 		<div class="form-group">
 			<label for="email" class="col-md-2">Indirizzo Email</label>
-			 <div class="col-md-10">
-			<input type="email" class="form-control" name="email" id="email" placeholder="Iserisci la tua email">
+			<div class="col-md-10">
+				<input type="email" class="form-control" name="email" id="email"
+					placeholder="Iserisci la tua email">
 			</div>
 		</div>
 		<div class="form-group">
 			<label for="password" class="col-md-2">Password</label>
-			 <div class="col-md-10">
-			<input type="password" class="form-control" name="password" id="password" placeholder="La tua password">
+			<div class="col-md-10">
+				<input type="password" class="form-control" name="password"
+					id="password" placeholder="La tua password">
 			</div>
 		</div>
 		<div class="form-group">
 			<label for="password_check" class="col-md-2">Conferma la password</label>
-			 <div class="col-md-10"> 	 
-			<input type="password" class="form-control" name="password_check" id="password_check" placeholder="Digita di nuovo la password">
+			<div class="col-md-10">
+				<input type="password" class="form-control" name="password_check"
+					id="password_check" placeholder="Digita di nuovo la password">
 			</div>
 		</div>
 		<div class="form-group">
 			<label for="ragionesociale" class="col-md-2">Ragione Sociale</label>
-			 <div class="col-md-10">
-			<input type="text" class="form-control" name="ragionesociale" id="ragionesociale" placeholder="Il nome dell'azienda">
+			<div class="col-md-10">
+				<input type="text" class="form-control" name="ragionesociale"
+					id="ragionesociale" placeholder="Il nome dell'azienda">
 			</div>
 		</div>
 		<div class="form-group">
 			<label for="partitaiva" class="col-md-2">Partita IVA</label>
-			 <div class="col-md-10">
-			<input type="text" class="form-control" name="partitaiva" id="partitaiva" placeholder="Inserisci la Partita IVA">
+			<div class="col-md-10">
+				<input type="text" class="form-control" name="partitaiva"
+					id="partitaiva" placeholder="Inserisci la Partita IVA">
 			</div>
 		</div>
 		<div class="form-group">
 			<label for="indirizzo" class="col-md-2">Indirizzo</label>
-			 <div class="col-md-6">
-			<input type="text" class="form-control" id="indirizzo" placeholder="Inserisci il tuo indirizzo">
+			<div class="col-md-6">
+				<input type="text" class="form-control" id="indirizzo"
+					placeholder="Inserisci il tuo indirizzo">
 			</div>
 			<label for="numero_civico" class="col-md-2">N° civico</label>
-			 <div class="col-md-2">
-			<input type="text" class="form-control" id="numero_civico" placeholder="N° civico">
+			<div class="col-md-2">
+				<input type="text" class="form-control" id="numero_civico"
+					placeholder="N° civico">
 			</div>
 		</div>
-		
+
 		<div class="form-group">
 			<label for="cap" class="col-md-2">CAP</label>
-			 <div class="col-md-3">
-			<input type="text" class="form-control" id="cap" placeholder="Codice Avviamento Postale">
+			<div class="col-md-3">
+				<input type="text" class="form-control" id="cap"
+					placeholder="Codice Avviamento Postale">
 			</div>
 			<label for="citta" class="col-md-1">Città</label>
-			 <div class="col-md-6">
-			<input type="text" class="form-control" id="citta" placeholder="Inserisci la città di residenza">
+			<div class="col-md-6">
+				<input type="text" class="form-control" id="citta"
+					placeholder="Inserisci la città di residenza">
 			</div>
 		</div>
-		
+
 		<div class="form-group">
 			<label for="provincia" class="col-md-2">Provincia</label>
-			 <div class="col-md-10">
-			<input type="text" class="form-control" id="provincia" placeholder="Inserisci la provincia">
+			<div class="col-md-10">
+				<input type="text" class="form-control" id="provincia"
+					placeholder="Inserisci la provincia">
 			</div>
 		</div>
-		
+
 		<div class="form-group">
 			<label for="cap" class="col-md-2">Telefono</label>
-			 <div class="col-md-4">
-			<input type="text" class="form-control" id="telefono" placeholder="Inserisci il tuo telefono">
+			<div class="col-md-4">
+				<input type="text" class="form-control" id="telefono"
+					placeholder="Inserisci il tuo telefono">
 			</div>
 			<label for="citta" class="col-md-2">Cellulare</label>
-			 <div class="col-md-4">
-			<input type="text" class="form-control" id="citta" placeholder="Inserisci un numero attivo">
+			<div class="col-md-4">
+				<input type="text" class="form-control" id="citta"
+					placeholder="Inserisci un numero attivo">
 			</div>
 		</div>
-		
-		<button type="submit" name="submit" class="btn btn-default">
-			Invia richiesta
-		</button>
+
+		<button type="submit" name="submit" class="btn btn-default">Invia
+			richiesta</button>
 	</form>
 
 </div>
 <!-- /container -->
 
 
- 
-		<?php
-		# if there are errors, they would be displayed here.
-		if (empty($errors) === false) {
-			echo '<p>' . implode('</p><p>', $errors) . '</p>';
-		}
-	?>
+
+<?php
+# if there are errors, they would be displayed here.
+if (empty($errors) === false) {
+	echo '<p>' . implode('</p><p>', $errors) . '</p>';
+}
+?>
