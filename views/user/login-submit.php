7a89296a5ed92dfe867b2dc9b386f4f46d839a02
jekyll86 <biagio.sesta@gmail.com>
2013-10-17 10:33:36 +0200

    fix user login

--- views/user/login-submit.php
+++ views/user/login-submit.php
@@ -7,7 +7,7 @@ if(empty($_POST)===false) {
 	$password=$_POST['password'];
 	if(empty($email) || empty($password)) {
 		$errors[]='Devi inserire le tua email e la tua password.';
-	} else if($users->email_exists($user_login)===false && $users->user_exists($user_login)) {
+	} else if($users->email_exists($user_login)===false && $users->user_exists($user_login) === false) {
 		$errors[]='Spiacenti l\'utente non esiste.';
 	}/* else if($users->email_confirmed($username)===false) {
 	$errors[]='Sorry, but you need to activate your account.  //per ora tutti gli utenti sono attivi di default
